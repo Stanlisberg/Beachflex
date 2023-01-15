3c1f8fcca176e5af1979d33bd33e7ea5d12be44a
Stanlisberg <nnabugo2010@gmail.com>
2023-01-15 23:30:58 +0100

    navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,64 @@
-import { FaBars} from 'react-icons/fa';
 import { BsChatSquareDots } from 'react-icons/bs'
 import {
     FaFacebook,
     FaTwitter,
-    FaGoogle,
+    FaGooglePlusG,
     FaInstagram,
+    FaBars,
 } from 'react-icons/fa'
+import { useState} from 'react';
 
+
+const Navbar = () => {
+    const [ nav, setNav ] = useState(false);
+}
 function Navbar() {
     return(
-        <div>
-            Navbar
+        <div className=' w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80'>
+           <ul className='hidden sm:flex px-4'>
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/gallery'>Gallery</a>
+                </li>
+                <li>
+                    <a href='/deals'>Deals</a>
+                </li>
+                <li>
+                    <a href='/contact'>Contact</a>
+                </li>
+           </ul>
+           <div className='flex justify-between'>
+              <FaFacebook className='mx-4' />
+              <FaTwitter className='mx-4' />
+              <FaGooglePlusG className='mx-4' />
+              <FaInstagram className='mx-4' />
+           </div>
+            {/* {hamburger Icon } */}
+           <div className='sm:hidden z-10'>
+             <FaBars size={20} className='mr-4 cursor-pointer'/>
+           </div>
+              {/* Mobile Menu */}
+           <div className={
+                  nav 
+                  ? 'overflow-y-hidden md:hidden ease-in duration absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col'
+           }
+                <ul>
+                    <li>
+                        <a href='/'>Home</a>
+                    </li>
+                    <li>
+                        <a href='/gallery'>Gallery</a>
+                    </li>
+                    <li>
+                        <a href='/deals'>Deals</a>
+                    </li>
+                    <li>
+                        <a href='/contact'>Contact</a>
+                    </li>
+                </ul>
+           </div>
         </div>
     )
 }
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -11,11 +11,11 @@ const TopBar = () => {
                 <h1> BeachFlex</h1>
             </div>
             <div className='flex'>
-                <div className='hidden md:flex items-center px-6'>
+                <div className='hidden md:flex items-center  px-6'>
                    <FaCoins size={20} className='mr-2 text-[var(--primary-dark)]'/>
                    <p className='text-sm text-gray-700'>9AM - 5AM</p>
                 </div>
-                <div className='hidden md:flex  items-center px-6'>
+                <div className='hidden md:flex  items-center  px-6'>
                     <AiFillPhone  size={20} className='mr-2 text-[var(--primary-dark)]'/>
                     <p className='text-sm text-gray-700'>1-888-734-3424</p>
                 </div>
