1bbf0af818a9e377fcf4c97701685dec860a03f5
Stanlisberg <nnabugo2010@gmail.com>
2023-01-16 18:39:27 +0100

    form design

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,10 +1,10 @@
 function Booking() {
     return(
-        <div>
-            <form>
-                <div>
+        <div id='deals' className='max-w-[1140px] m-auto w-full p-4'>
+            <form className='lg:flex lg:justify-between w-full items-center'>
+                <div className='flex flex-col py-2 my-2'>
                     <label>Destination</label>
-                    <select>
+                    <select className='lg:w-[300px] md:w-full border rounded-md p-2'>
                         <option>Grande Antigua</option>
                         <option>Key West</option>
                         <option>Maldives</option>
